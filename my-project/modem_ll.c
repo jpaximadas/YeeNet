5150e2118fc1c21c0024eb2a844e3d105b82916b
Jason <jasonpax@protonmail.com>
2020-05-03 12:31:34 -0500

    fixed issue with preamble lsb and msb being swapped with each other

--- my-project/modem_ll.c
+++ my-project/modem_ll.c
@@ -225,8 +225,12 @@ void lora_config_modulation(struct modem *this_modem, struct modulation_config *
     lora_write_reg(this_modem,LORA_REG_MODEM_CONFIG_2,temp);
 
     //set preamble length
-    uint8_t top = (uint8_t) modulation->preamble_length;
-    uint8_t bot = (uint8_t) (modulation->preamble_length >> 8);
+    uint8_t bot = (uint8_t) modulation->preamble_length;
+    uint8_t top = (uint8_t) (modulation->preamble_length >> 8);
+    fprintf(fp_uart,"preamble msb:%x",top);
+    fprintf(fp_uart,"preamble lsb:%x",bot);
+    
+   
     lora_write_reg(this_modem,LORA_REG_PREAMBLE_MSB,top);
     lora_write_reg(this_modem,LORA_REG_PREAMBLE_LSB,bot);
 
