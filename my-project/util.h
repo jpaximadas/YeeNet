74c151649b8f786070c5d1c6c563a7a6ed05657c
Jason <jasonpax@protonmail.com>
2020-05-26 23:22:00 -0500

    moved systick utilization out of util

--- my-project/util.h
+++ my-project/util.h
@@ -1,7 +1,5 @@
 #pragma once
 #include <sys/types.h>
-#include <libopencm3/cm3/systick.h>
-#define TIMER_OFFSET 999
 
 inline static void delay_nops(uint32_t nops){
 	for(uint32_t i = 0; i<nops;i++){
@@ -13,21 +11,3 @@ struct pin_port {
 	uint32_t port;
 	uint32_t pin;
 };
-
-extern volatile uint32_t ms;
-
-void timer_setup(void);
-
-static inline uint32_t timer_get_micros(void){
-	uint32_t ms_latch = ms;
-	uint32_t systick_counter_latch = systick_get_value();
-	double micros = systick_counter_latch/71999.0;
-	micros = micros * 1000;
-	uint32_t retval = (ms_latch*1000) + ((uint32_t)micros);
-	return retval;
-}
-
-static inline void timer_reset(void){
-	ms = 0;
-	systick_clear();
-}
